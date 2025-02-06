da29353a45a0e83d47e5654496f9462873a5045d
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-02-06 00:37:22 -0600

    Time: 15 ms (82.33%) | Memory: 18.1 MB (10.91%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,14 +3,13 @@ class Solution:
         if(len(s)==0 or len(s)==1):
             return len(s)
         t=s[0]
-
         long_str=0
         for i in range(1,len(s)):
             if s[i] in t:
                 if(len(t)>0):
-                        if(len(t)>long_str):
-                            long_str=len(t)
-                        t=t.split(s[i])[1]
+                    if(len(t)>long_str):
+                        long_str=len(t)
+                    t=t.split(s[i])[1]
             t= t+s[i]
         if(len(t)>long_str):
             long_str=len(t) 
