6c945e0bc072bbbce982a60168b369cc65f19425
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-02-06 00:47:00 -0600

    Time: 7 ms (63.75%) | Memory: 18 MB (17.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,14 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        s_x=str(x)
         if x < 0 or (x % 10 == 0 and x != 0):  # Negative numbers and numbers ending in 0 (except 0) are not palindromes
             return False
-        return str(x) == str(x)[::-1]
-        # str_x=str(x)
-        # t_len=len(str_x)
-        # for i in range(0,t_len):
-        #     if(str_x[i]!=str_x[t_len-i-1]):
-        #         return False
-        # return True
+        return s_x == s_x[::-1]
 
         
 
