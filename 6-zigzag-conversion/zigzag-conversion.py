14e355be7da3b1c07b2e7ef8c98dff18bb0dd69e
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-06-18 18:10:57 -0500

    Time: 11 ms (65.01%) | Memory: 17.9 MB (70.41%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -3,18 +3,16 @@ class Solution:
         if(numRows==1):
           return s
         res=[[] for _ in range(numRows)]
-        print(res)
+
         j=0
         downwards=1
 
         for i in range(len(s)):
             
-            print(j, downwards, s[i])
             res[j].append(s[i])
             
             if(j==(numRows-1)):
                 downwards=-1
-                print(res)
             if(j==0):
                 # print(j, downwards)
                 downwards=1
