58bc26181c431ae99f897c33f108dd7529a41bcd
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-06-18 18:09:40 -0500

    Time: 69 ms (10.69%) | Memory: 18 MB (31.61%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -3,7 +3,7 @@ class Solution:
         res=[[] for _ in range(numRows)]
         print(res)
         j=0
-        downwards=True
+        downwards=1
         if(numRows==1):
             return s
         for i in range(len(s)):
@@ -12,15 +12,12 @@ class Solution:
             res[j].append(s[i])
             
             if(j==(numRows-1)):
-                downwards=False
+                downwards=-1
                 print(res)
             if(j==0):
                 # print(j, downwards)
-                downwards=True
-        
-            if(downwards):
-                j=j+1
-            else:
-                j=j-1
+                downwards=1
+            j=j+downwards
+
         return ''.join(char for sublist in res for char in sublist)
         
