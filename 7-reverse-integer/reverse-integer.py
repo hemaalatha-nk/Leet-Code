bbc99be690d6de238b11ac32fb5dc3902bca561b
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-06-18 20:35:33 -0500

    Time: 34 ms (85.10%) | Memory: 17.7 MB (56.91%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,18 +1,35 @@
 class Solution:
     def reverse(self, x: int) -> int:
-            res=0
-            temp_x=x
-            min_x= -2 ** 31 
-            max_x=(2 ** 31) -1
-            if((x< min_x ) or( x> max_x)):
-                return 0
-            while temp_x!=0:
-                if(temp_x>0):
-                    res=(res*10) + (temp_x%10)
-                else:
-                    res=(res*10) + (temp_x%-10)
-                temp_x=int(temp_x/10)
+        min_x= -2 ** 31 
+        max_x=(2 ** 31) -1
+        if((x< min_x ) or( x> max_x)):
+            return 0
+        if(x<0):
+            x=x*-1
+            sx=f"{x}"
+            res=int(sx[::-1]) * -1
             if((res< min_x ) or( res>max_x)):
                 return 0
             return res
+        else:
+            sx=f"{x}"
+            res=int(sx[::-1])
+            if((res< min_x ) or( res>max_x)):
+                return 0
+            return res
+            # res=0
+            # temp_x=x
+            # min_x= -2 ** 31 
+            # max_x=(2 ** 31) -1
+            # if((x< min_x ) or( x> max_x)):
+            #     return 0
+            # while temp_x!=0:
+            #     if(temp_x>0):
+            #         res=(res*10) + (temp_x%10)
+            #     else:
+            #         res=(res*10) + (temp_x%-10)
+            #     temp_x=int(temp_x/10)
+            # if((res< min_x ) or( res>max_x)):
+            #     return 0
+            # return res
         
