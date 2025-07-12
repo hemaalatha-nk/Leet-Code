7cc31e5bf71b74280f7df7f23dfa3693f0f53c6b
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-11 20:01:29 -0500

    Time: 45 ms (12.93%) | Memory: 17.9 MB (8.22%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -6,8 +6,8 @@ class Solution:
         while i<len(word1) and j<len(word2):
             res.append(word1[i])
             res.append(word2[j])
-            i=i+1
-            j=j+1
+            i+=1
+            j+=1
         res.append(word1[i:])
         res.append(word2[j:])
         return "".join(res)
