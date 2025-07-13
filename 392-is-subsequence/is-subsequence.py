2b905c76f55eac35547acf29c34af91d71d24be5
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-13 11:28:44 -0500

    Time: 3 ms (25.37%) | Memory: 17.7 MB (91.41%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -4,17 +4,14 @@ class Solution:
 
         s_p=0
         t_p=0
-   
 
         while s_p<len(s) and t_p<len(t):
             # print(s_p,t_p)
             if(s[s_p]==t[t_p]):
                 s_p+=1
             t_p+=1
-        if(s_p>=len(s)):
-            return True
-        else:
-            return False
+
+        return s_p==len(s)
 
 
 
