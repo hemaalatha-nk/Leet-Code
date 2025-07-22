cfe2d362bccbb2f70b730c1c5b2990cd5130774d
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-22 11:16:28 -0500

    Time: 34 ms (84.49%) | Memory: 27 MB (32.38%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,12 +6,12 @@ class Solution:
 
         for i in range(1, len(prices)):
             if (prices[i]< prices[min]):
-                print(i)
+                # print(i)
                 min=i
                 if(max < min):
                     max=min
             elif(prices[i]>prices[max]):
-                print(i)
+                # print(i)
                 max=i
                 if(min<max):
                     if(result< prices[max]- prices[min] ):
