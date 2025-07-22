77be3ce9f2450195d4ef2d9f56f9253a95b00d6e
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-21 23:59:39 -0500

    Time: 66 ms (5.54%) | Memory: 19.8 MB (19.17%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        majority_ele={}
+        result=0
+        n=len(nums)/2
+
+        for i in range(0,len(nums)):
+            if nums[i] in majority_ele:
+                majority_ele[nums[i]]=  majority_ele[nums[i]]+1
+            else:
+                 majority_ele[nums[i]]=1
+        
+            if(majority_ele[nums[i]]>n):
+                n=majority_ele[nums[i]]
+                result=nums[i]
+            print(majority_ele[nums[i]] )
+        return result
+
+
+        
