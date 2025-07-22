32841a42004e3ba3b7388bbf60cf1e5e4e984677
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-22 11:30:46 -0500

    Time: 33 ms (84.97%) | Memory: 26.9 MB (32.38%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,37 +1,37 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        # min = max=0
+        min = max=0
         result=0
 
-        l=0
-        r=1
+        # l=0
+        # r=1
 
-        while r<len(prices):
-            if(prices[l]< prices[r]):
-                profit= prices[r]- prices[l]
-                result=max(profit, result)
-            else:
-                l=r
-            r=r+1
-        return result
+        # while r<len(prices):
+        #     if(prices[l]< prices[r]):
+        #         profit= prices[r]- prices[l]
+        #         result=max(profit, result)
+        #     else:
+        #         l=r
+        #     r=r+1
+        # return result
 
             
 
-        # for i in range(1, len(prices)):
-        #     if (prices[i]< prices[min]):
-        #         # print(i)
-        #         min=i
-        #         if(max < min):
-        #             max=min
-        #     elif(prices[i]>prices[max]):
-        #         # print(i)
-        #         max=i
-        #         if(min<max):
-        #             if(result< prices[max]- prices[min] ):
-        #                 result= prices[max]- prices[min]
+        for i in range(1, len(prices)):
+            if (prices[i]< prices[min]):
+                # print(i)
+                min=i
+                if(max < min):
+                    max=min
+            elif(prices[i]>prices[max]):
+                # print(i)
+                max=i
+                if(min<max):
+                    if(result< prices[max]- prices[min] ):
+                        result= prices[max]- prices[min]
 
-        # return result
+        return result
 
 
 
