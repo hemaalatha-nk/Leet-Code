b78a9d020ea1f93adad3e8900a398a3622070bd6
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-22 19:44:33 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (70.72%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -6,20 +6,23 @@ class Solution:
         result=0
 
         for i in range(1,len(prices)):
-            if(prices[i]< prices[i-1]):
-                    high=i-1
-                    # low=i
-                    result=prices[high]-prices[low] +result
+            if(prices[i]> prices[i-1]):
+                result+=prices[i]-prices[i-1] 
 
-                    print(high, low, result)
-                    low=i
+            # if(prices[i]< prices[i-1]):
+            #         high=i-1
+            #         # low=i
+            #         result=prices[high]-prices[low] +result
 
-            elif(prices[low]>prices[i]):
-                low=i
+            #         print(high, low, result)
+            #         low=i
 
-            if(i+1==len(prices) and prices[low]<prices[i]):
-                    result=prices[i]-prices[low] +result
-                    print("case last, i", i)
+            # elif(prices[low]>prices[i]):
+            #     low=i
+
+            # if(i+1==len(prices) and prices[low]<prices[i]):
+            #         result=prices[i]-prices[low] +result
+            #         print("case last, i", i)
         
         # while high < len(prices):
         #     if(prices[low] > prices[high]):
