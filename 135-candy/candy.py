4faec403e3be548ea4e3db419321df907c16cdfd
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-25 15:15:51 -0500

    Time: 55 ms (5.07%) | Memory: 19.8 MB (69.61%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,21 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+
+        no_candy= [1]* len(ratings)
+
+        for i in range(0, len(ratings)-1):
+            if(ratings[i]<ratings[i+1]):
+                no_candy[i+1]=  no_candy[i]+1
+
+        print(no_candy)
+        for j in range(len(ratings)-1,0,-1):
+            print(j)
+            if(ratings[j]<ratings[j-1]):
+                no_candy[j-1]= max( no_candy[j-1], no_candy[j]+1)
+        
+        print(no_candy)
+
+        return sum(no_candy)
+        
+
+        
