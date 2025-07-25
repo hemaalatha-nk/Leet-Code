9ec0aa0757e912ee73394795d8845005119f5cd9
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-25 15:19:02 -0500

    Time: 47 ms (5.85%) | Memory: 20 MB (36.82%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,15 +3,25 @@ class Solution:
 
         no_candy= [1]* len(ratings)
 
-        for i in range(0, len(ratings)-1):
-            if(ratings[i]<ratings[i+1]):
-                no_candy[i+1]=  no_candy[i]+1
+        # for i in range(0, len(ratings)-1):
+        #     if(ratings[i]<ratings[i+1]):
+        #         no_candy[i+1]=  no_candy[i]+1
+
+        
+        for i in range(1, len(ratings)):
+            if(ratings[i-1]<ratings[i]):
+                no_candy[i]=  no_candy[i-1]+1
 
         # print(no_candy)
-        for j in range(len(ratings)-1,0,-1):
+        # for j in range(len(ratings)-1,0,-1):
+        #     print(j)
+        #     if(ratings[j]<ratings[j-1]):
+        #         no_candy[j-1]= max( no_candy[j-1], no_candy[j]+1)
+
+        for j in range(len(ratings)-2,-1,-1):
             print(j)
-            if(ratings[j]<ratings[j-1]):
-                no_candy[j-1]= max( no_candy[j-1], no_candy[j]+1)
+            if(ratings[j+1]<ratings[j]):
+                no_candy[j]= max( no_candy[j], no_candy[j+1]+1)
         
         # print(no_candy)
 
