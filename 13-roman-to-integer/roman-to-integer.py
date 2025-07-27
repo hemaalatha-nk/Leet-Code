ff350ca6c3b03d609012555f978a72193250c6fe
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-27 17:59:31 -0500

    Time: 8 ms (26.36%) | Memory: 18 MB (20.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
 
-        # s=list(s)
+     
         h_table={'I':1, 'V': 5, 'X':10, 'L':50,'C':100,'D':500,'M':1000}
         res=0
-        i=0
 
-        while i < len(s):
+        for i in range(len(s)):
             if(i+1<len(s) and h_table[s[i+1]]>h_table[s[i]]):
                 res-=h_table[s[i]]               
             else:
                 res+= h_table[s[i]]
-            i+=1
 
         
         return res
