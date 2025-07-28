56a66d0fed468d7d6be2480c7630e89d875e0363
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-28 13:07:02 -0500

    Time: 4 ms (48.02%) | Memory: 18.5 MB (90.26%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -12,39 +12,3 @@ class Solution:
             else:
                 left_p+=1
         return False
-
-
-
-
-        right_p= len(numbers)-1
-        if(right_p==1 and numbers[0]+numbers[1]==target):
-            return [1,2]
-        
-        left_p= 0
-        mid_p=len(numbers)/2-1
-        n=right_p
-
-        for i in range(0, len(numbers)):
-            left_p=i+1
-            right_p= len(numbers)-1
-            mid_p=int((left_p+2+right_p)/2)-1
-
-            while right_p >= left_p:
-
-                diffs=target-numbers[i]
-                if( diffs==numbers[mid_p]):
-                    return [1+i, 1+mid_p]
-                elif (diffs>numbers[mid_p]):
-                    left_p=mid_p+1
-                    mid_p=int((left_p+2+right_p)/2)-1
-                else:
-                    right_p=mid_p-1
-                    mid_p=int((left_p+2+right_p)/2)-1
-        return False
-
-
-
-
-
-
-        
