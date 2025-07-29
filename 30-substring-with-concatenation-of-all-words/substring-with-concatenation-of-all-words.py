6f1ff23088cb1aae0db394e78f9513dd7f29282b
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-07-29 11:54:23 -0500

    Time: 4392 ms (44.90%) | Memory: 18.8 MB (37.77%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+
+        l_w=len(words[0])
+        l=0
+        r= l_w*len(words)
+        ans=[]
+
+        while l<len(s) and r<=len(s):
+            temp=[]
+            tempw=s[l:r]
+
+            for t in range(0,len(tempw),l_w):
+                temp.append(tempw[t:t+l_w])
+            if sorted(temp)== sorted(words):
+                ans.append(l)
+            l+=1
+            r+=1
+        return ans
+
+
+
+
+
+    
+
+    
+
