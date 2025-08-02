bf988ab41f5835f26e05a32d1263e5012d1f78c6
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-02 11:20:52 -0500

    Time: 63 ms (30.04%) | Memory: 33.4 MB (73.75%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,15 +3,12 @@ class Solution:
         l=0
         ans=0
         nums=list(sorted(set(nums)))
-        print(nums,sorted(nums))
         if len(nums)==0:
             return 0
 
         for r in range(1,len(nums)):
-            print(nums[r-1], nums[r])
             if(nums[r-1]+1==nums[r]):
                 ans=max(r-l,ans)
-                print(True)
                 continue
             else:
                 l=r
