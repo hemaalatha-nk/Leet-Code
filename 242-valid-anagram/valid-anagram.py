cdc3a0786fc625b30e2a7cd72c1ce4367451116f
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-02 08:54:18 -0500

    Time: 4 ms (94.17%) | Memory: 17.9 MB (64.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
+        if(len(s)!=len(t)):
+            return False
 
-        return sorted(s)==sorted(t)
+
+        return collections.Counter(s)==collections.Counter(t)
 
         
