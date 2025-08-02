ef3e22cf1c974b5b1b38a3c15391884f6f3462a4
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-02 09:20:38 -0500

    Time: 11 ms (82.39%) | Memory: 20.5 MB (89.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,19 +4,15 @@ class Solution:
         if(len(strs)==1):
             return[strs]
         hash_map={}
-        # print("".join(sorted(strs[0])), strs)
         for s in strs:
             _s="".join(sorted(s))
-            print(_s,s,_s not in hash_map)
             if(_s not in hash_map):
-
                 hash_map[_s] = []
                 hash_map[_s].append(s)
             else:
                 strs
                 hash_map[_s].append(s)
 
-        print(hash_map)
         return list(hash_map.values())
 
         
