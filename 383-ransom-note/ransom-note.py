6012ed61e72b463ee541a0b51840f4beb37e945c
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-02 00:37:45 -0500

    Time: 15 ms (75.28%) | Memory: 18.1 MB (34.47%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,8 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
+        if len(ransomNote)<=0:
+            return False
+
+
         mag= {}
         rag={}
+
 
         for r in ransomNote:
             rag[r]=rag.get(r,0)+1
@@ -12,3 +17,6 @@ class Solution:
                 rag[m]-=1
 
         return (all(value == 0 for value in rag.values()))
+
+        # ran=list(ransomNote)
+        
