32c00eb2974de103eda3fc5aa53dee2f2dc1fe75
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-01 20:53:06 -0500

    Time: 17 ms (59.93%) | Memory: 18 MB (34.47%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -4,28 +4,11 @@ class Solution:
         mag= {}
         rag={}
 
-        # for m in magazine:
-        #     mag[m]=mag.get(m,0)+1
-        # print(mag)
-
         for r in ransomNote:
             rag[r]=rag.get(r,0)+1
-
-
-        # for r in ransomNote:
-        #     if r in mag and mag[r]>0:
-        #         mag[r]-=1
-        # print("mag2: ",mag)
 
         for m in magazine:
             if m in rag and rag[m]>0:
                 rag[m]-=1
-        print("rag2: ",rag)
 
         return (all(value == 0 for value in rag.values()))
-
-        if ransomNote in magazine:
-            return True
-        return False
-            
-        
