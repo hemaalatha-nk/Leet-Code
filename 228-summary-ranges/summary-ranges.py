1f52f56f6a1368f3f8b3452597b79fdbb29f54bb
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-02 13:37:40 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.68%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,29 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+
+        ans=[]
+        start=0
+        end=0
+        if len(nums)==1:
+            return [str(nums[0])]
+
+        for i in range(0,len(nums)-1):
+            print(nums[i]+1,nums[i+1],start,i)
+            if(nums[i]+1!=nums[i+1]):
+                if(start==i):
+                    ans.append(f'{nums[start]}')
+                else:
+                    ans.append(f"{nums[start]}->{nums[i]}")
+                start=i+1
+                if(start==len(nums)-1):
+                    ans.append(f'{nums[start]}')
+            else:
+                print("lol",i,len(nums)-2 , start)
+                if(i==len(nums)-2 ):
+                    ans.append(f"{nums[start]}->{nums[i+1]}")
+
+                
+        return(ans)
+
+
+        
