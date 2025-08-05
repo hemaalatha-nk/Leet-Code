b86f7e8c9bc6333cb80030f98d6c6564dea0376d
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-05 15:40:39 -0500

    Time: 3 ms (5.20%) | Memory: 17.8 MB (68.99%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,19 +1,44 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
-        if not digits:
-            return []
         
-        digit_to_chars = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
-        combinations=[""]
+        if len(digits) == 0:
+            return []
+        else:
+            digitMap = { 
+                "2" : "abc",
+                "3" : "def",
+                "4" : "ghi",
+                "5" : "jkl",
+                "6" : "mno",
+                "7" : "pqrs",
+                "8" : "tuv",
+                "9" : "wxyz"
+            }
+            res = []
+            def phoneNumber(indx : int, currComb : list[str]):
+                if len(currComb) == len(digits):
+                    res.append("".join(currComb))
+                    return
+                for c in digitMap[digits[indx]]:
+                    currComb.append(c)
+                    phoneNumber(indx + 1, currComb)
+                    currComb.pop()
+            phoneNumber(0, [])
+            return res
 
-        for digit in digits:
+        # if not digits:
+        #     return []
+        
+        # digit_to_chars = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
+        # combinations=[""]
 
-            letters=digit_to_chars[int(digit)]
+        # for digit in digits:
 
-            combinations=[prefix + letter for prefix in combinations for letter in letters]
-            print(combinations)
+        #     letters=digit_to_chars[int(digit)]
 
-        return combinations
+        #     combinations=[prefix + letter for prefix in combinations for letter in letters]
+        #     print(combinations)
+
+        # return combinations
 
         
