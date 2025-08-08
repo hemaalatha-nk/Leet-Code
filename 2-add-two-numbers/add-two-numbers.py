b59eb9fe7520d980dc641cb341034e25a7dcd9a1
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-08-07 23:05:52 -0500

    Time: 4 ms (55.46%) | Memory: 18.3 MB (14.59%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -6,37 +6,55 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        n1=""
-        n2=""
+        dummy=ListNode()
+        current=dummy
+        carry=0
 
-        list1=l1
-        list2=l2
+        while l1 or l2 or carry:
 
-        while list1!=None or list2!=None:
+            sum_= (l1.val if l1 else 0 )+ (l2.val if l2 else 0) + carry
+            carry= sum_ // 10
+            # current.val=sum_%10
+            current.next=ListNode(sum_%10)
+            current=current.next
+            print(carry)
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
+        return dummy.next
 
-            if list1!=None:
-                n1=str(list1.val)+n1
-                list1=list1.next
-            if list2!=None:
-                n2=str(list2.val)+n2
-                list2=list2.next
+        # n1=""
+        # n2=""
+
+        # list1=l1
+        # list2=l2
+
+        # carry=0
+
+        # while list1!=None or list2!=None:
+
+        #     if list1!=None:
+        #         n1=str(list1.val)+n1
+        #         list1=list1.next
+        #     if list2!=None:
+        #         n2=str(list2.val)+n2
+        #         list2=list2.next
         
 
-        n1=str(int(n1)+int(n2))
+        # n1=str(int(n1)+int(n2))
 
 
-        res=ListNode()
-        cur=res
+        # res=ListNode()
+        # cur=res
 
-        for i in range(len(n1),0,-1):
-            cur.val=int(n1[i-1:i])
-            if(i!=1):
-                c1=ListNode()
-                cur.next=c1
-                cur=c1
+        # for i in range(len(n1),0,-1):
+        #     cur.val=int(n1[i-1:i])
+        #     if(i!=1):
+        #         c1=ListNode()
+        #         cur.next=c1
+        #         cur=c1
             
             
-        return res
+        # return res
 
 
 
