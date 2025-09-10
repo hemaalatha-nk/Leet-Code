67273f8b8af29331e9f87aa297a9a0e9f57c6e11
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2025-09-10 10:27:52 -0500

    Time: 767 ms (53.89%) | Memory: 18.3 MB (43.22%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,37 +1,37 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        # coins.sort()
-        # dp_=[0]*(amount+1)
+        coins.sort()
+        dp_=[0]*(amount+1)
 
-        # for i in range(0,amount+1):
-        #     minn=float('inf')
+        for i in range(1,amount+1):
+            minn=float('inf')
 
-        #     for c in coins:
+            for c in coins:
 
-        #         diff= i-c
-        #         if diff<0:
-        #             break
-        #         minn= min(minn, dp_[diff]+1)
+                diff= i-c
+                if diff<0:
+                    break
+                minn= min(minn, dp_[diff]+1)
 
-        #     dp_[i]=minn
-        # if dp_[amount]<float('inf'):
-        #     return dp_[amount]
-        # else:
-        #     return -1
+            dp_[i]=minn
+        if dp_[amount]<float('inf'):
+            return dp_[amount]
+        else:
+            return -1
 
       
-        coins.sort()
-        dp_ = [float('inf')] * (amount + 1)
-        dp_[0] = 0  # Base case
+        # coins.sort()
+        # dp_ = [float('inf')] * (amount + 1)
+        # dp_[0] = 0  # Base case
 
-        for i in range(1, amount + 1):
-            minn = float('inf')
-            for c in coins:
-                diff = i - c
-                if diff < 0:
-                    break
-                minn = min(minn, dp_[diff] + 1)
-            dp_[i] = minn  # ✅ store minn
+        # for i in range(1, amount + 1):
+        #     minn = float('inf')
+        #     for c in coins:
+        #         diff = i - c
+        #         if diff < 0:
+        #             break
+        #         minn = min(minn, dp_[diff] + 1)
+        #     dp_[i] = minn  # ✅ store minn
 
-        return dp_[amount] if dp_[amount] != float('inf') else -1
+        # return dp_[amount] if dp_[amount] != float('inf') else -1
