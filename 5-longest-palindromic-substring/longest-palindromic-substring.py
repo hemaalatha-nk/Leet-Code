806d8a38d77a0002b71fd71b2a4d5ba01b70dbea
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2026-02-04 00:02:12 -0600

    Time: 207 ms (92.41%) | Memory: 19.5 MB (28.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        r=0
-        l=len(s)
-        prevp=""
-        if len(s)==1:
+        n = len(s)
+        if n < 2:
             return s
 
-        while l>r:
-            p=s[r:l]
-            # print(p)
-            # print(l,r+1)
-            if p==p[::-1]:
-                if len(prevp)<len(p):
-                    prevp=p
-                r=r+1
-                l=len(s)
-                    
-            elif l!=r+2:
-                l=l-1
-            else:
-                r=r+1
-                l=len(s)
-        return prevp
-        # if len(s)==1:
-        #     return s
+        best_l = best_r = 0  # inclusive indices of best palindrome
 
-       
+        def expand(l: int, r: int):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r - 1  # step back to last valid
+
+        for i in range(n):
+            # odd length center at i
+            l1, r1 = expand(i, i)
+            if r1 - l1 > best_r - best_l:
+                best_l, best_r = l1, r1
+
+            # even length center between i and i+1
+            l2, r2 = expand(i, i + 1)
+            if r2 - l2 > best_r - best_l:
+                best_l, best_r = l2, r2
+
+        return s[best_l:best_r + 1]
