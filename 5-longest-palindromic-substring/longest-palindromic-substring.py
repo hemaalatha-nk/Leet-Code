3e3b02dd415bdf04dd1d129dda961ff88ecfac71
hemaalatha-nk <143139368+hemaalatha-nk@users.noreply.github.com>
2026-02-03 23:47:13 -0600

    Time: 5671 ms (9.24%) | Memory: 19.3 MB (44.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,27 +1,28 @@
 class Solution:
-    def checking_palin(self, sub_str):
-        while sub_str!=sub_str[::-1]:
-            sub_str=sub_str[1:]
-        if(len(sub_str)>1):
-            if (sub_str==sub_str[ : :-1]):
-                return sub_str
-
     def longestPalindrome(self, s: str) -> str:
-        if(len(s)==1):
-            return s
-        ans=None
-        sub_str=s[0]
-
-        for i in range(1, len(s)):
-            sub_str=sub_str+s[i]
-            a=self.checking_palin(sub_str)
-            if(a!=None):
-                if(ans==None or len(a)>=len(ans)):
-                    ans=a
-
-        if ans!=None:
-                return ans
-        else:
-            return s[0]
-
         
+        r=0
+        l=len(s)
+        print(s[r:l])
+        prevp=""
+
+        while l>r:
+            p=s[r:l]
+            # print(p)
+            # print(l,r+1)
+            if p==p[::-1]:
+                if len(prevp)<len(p):
+                    prevp=p
+                r=r+1
+                l=len(s)
+                    
+            elif l!=r+2:
+                l=l-1
+            else:
+                r=r+1
+                l=len(s)
+        return prevp
+        # if len(s)==1:
+        #     return s
+
+       
